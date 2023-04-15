9d3e292acf93881ff34dc6fcdf2dcfe1c2b3da83
Azamat Sultonov <azamatsultanov314@gmail.com>
2023-04-15 15:26:08 +0300

    removed NLRT

--- src/alternating_projections.py
+++ src/alternating_projections.py
@@ -1,22 +1,8 @@
 import numpy as np
 import truncated_svd
+import utils
 from tensor_train import TensorTrain
 
-
-def NLRT(tensor, ranks, itersNum, truncatedSvd=truncated_svd.SVDr):
-    '''https://arxiv.org/abs/2007.14137v2'''
-    m = len(tensor.shape)
-    X = [tensor.copy()] * m
-    for _ in range(itersNum):
-        # projection onto \Omega_1
-        for Xi in X:
-            Xi[Xi < 0] = 0
-        Yi = sum(X) / m
-        # projection onto \Omega_2
-        for i in range(m):
-            u, vh = truncatedSvd(Unfold(Yi, i), ranks[i])
-            X[i] = Fold(u @ vh, i, tensor.shape)
-    return X
 
 def NTTSVD(tensor, ranks, itersNum, truncatedSvd=truncated_svd.SVDr):
     '''https://arxiv.org/abs/2209.02060'''
